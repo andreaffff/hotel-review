de925072754974330e3dd2a76c7832680aa79bba
VALERIO VICCA <v.vicca@studenti.unimol.it>
2022-08-09 19:30:17 +0200

    signin with controls and errors in json

--- src/main/java/hotelReviewBackend/controller/UserController.java
+++ src/main/java/hotelReviewBackend/controller/UserController.java
@@ -2,21 +2,26 @@ package hotelReviewBackend.controller;
 
 import hotelReviewBackend.JDBC.JDBC;
 import hotelReviewBackend.Model.UserModel;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
-import java.security.SecureRandom;
+import javax.ws.rs.core.Response;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserController {
-    public static UserModel addUser(UserModel user) {
+    public static Response addUser(UserModel user) {
         Connection connection = JDBC.getInstance().getConnection();
+        JSONObject object = null;
+        Response response = null;
         //Questi due controlli li faccio qui o nei getter di password e di role ?
         String role;
         String hash = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(10));
-        ResultSet result;
+        ResultSet result = null;
 
         if (user.getRole() != "worker" && user.getRole() != "admin")
             role = "worker";
@@ -26,14 +31,13 @@ public class UserController {
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(checkUser);
             result = preparedStatement.executeQuery();
-            while (result.next())
-                System.out.println(result.getString("username"));
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
+            //throw new RuntimeException(e);
         }
-
-        if (result == null) {
+        System.out.println("result:"+result);
+        if (result == null) { //TODO da capire bene come funziona
             System.out.println("entra qui");
             String sql = "INSERT INTO users (username,name,surname,address,password,phone,role,email) VALUES (?,?,?,?,?,?,?,?)";
             try {
@@ -49,13 +53,33 @@ public class UserController {
                 preparedStatement.setString(7, role);
                 preparedStatement.setString(8, user.getEmail());
                 preparedStatement.executeUpdate();
-            } catch (SQLException e) {
+                object = new JSONObject();
+                object.put("Avviso", "Utente registrato correttamente"); //verificare se è meglio try/catch o aggiungere il metode in signature
+                response = Response.status(Response.Status.OK).entity(object.toString()).build();
+                //System.out.println("Utente registrato correttamente");
+            } catch (SQLException e) { //TODO controllare cosa scrivere nelle parentesi
                 throw new RuntimeException(e);
+            } catch (JSONException e) { //sta qui per object.put
+                e.printStackTrace();
             }
-        }else
-            System.out.println("Utente già registrato"); //Da inserire come risposta json
-
-        JDBC.closeConnection(connection);
-        return user;
+        } else {
+            try {
+                if(result.getString("username")== user.getUsername()) {
+                    try {
+                        object = new JSONObject();
+                        object.put("Avviso", "Utente già registrato"); //verificare se è meglio try/catch o aggiungere il metode in signature
+                        response = Response.status(Response.Status.CONFLICT).entity(object.toString()).build();
+                        // System.out.println("Utente già registrato"); //Da inserire come risposta json
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        Response.status(400).entity(object).build();
+            JDBC.closeConnection(connection);
+            return response;
+        }
     }
-}
